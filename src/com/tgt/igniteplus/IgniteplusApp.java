fce5514c755afd83e2e8d9eba279c06fc1eb6dc3
ramya2499 <2662ramya@gmail.com>
2020-03-21 17:14:10 +0530

    ram

--- src/com/tgt/igniteplus/IgniteplusApp.java
+++ src/com/tgt/igniteplus/IgniteplusApp.java
@@ -3,7 +3,7 @@ package com.tgt.igniteplus;
 public class IgniteplusApp {
 
     public static void main(String[] args) {
-        System.out.println("Welcome to Ingite Plus");
+        System.out.println("Welcome to Ignite");
     }
 
 }
